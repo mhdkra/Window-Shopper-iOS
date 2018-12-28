04dbf97c19fb57cab24113fc84045e82122963e3
Tiara Mahardika <tiaramahardika@student.ub.ac.id>
2018-12-28 10:48:26 +0700

    custom textfield

---
+++ Window-Shopper-iOS/Views/CurrencyTextField.swift
@@ -0,0 +1,26 @@
+//
+//  CurrencyTextField.swift
+//  Window-Shopper-iOS
+//
+//  Created by Tiara Mahardika on 28/12/18.
+//  Copyright © 2018 Tiara Mahardika. All rights reserved.
+//
+
+import UIKit
+
+class CurrencyTextField: UITextField {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 0.2480201199)
+        layer.cornerRadius = 5.0
+        textAlignment = .center
+        
+        if let p =  placeholder {
+            let place = NSAttributedString (string: p , attributes: [.foregroundColor: #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)])
+            attributedPlaceholder = place
+            textColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
+        }
+    }
+
+}
