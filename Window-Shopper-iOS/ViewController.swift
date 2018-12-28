6110c450112848b0e073cce4fa44d8068bea77fa
Tiara Mahardika <tiaramahardika@student.ub.ac.id>
2018-12-28 09:57:44 +0700

    initial commit 2

---
+++ Window-Shopper-iOS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Window-Shopper-iOS
+//
+//  Created by Tiara Mahardika on 28/12/18.
+//  Copyright © 2018 Tiara Mahardika. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
